09f072a008122c89897000268207647d24f17566
Ines103 <77297566+Ines103@users.noreply.github.com>
2021-09-30 15:23:20 +0200

    initial commit

--- StudentManagementSystem.js
+++ StudentManagementSystem.js
@@ -1,1 +1,47 @@
 
+var numberOfStudents= 0;
+function addStudent() {
+    return numberOfStudents= numberOfStudents+1 ;
+   }
+   console.log(numberOfStudents) 
+   addStudent()
+   console.log(numberOfStudents) 
+   addStudent()
+   console.log(numberOfStudents) 
+ 
+   function getNumberOfStudents() {
+    return numberOfStudents;
+   }
+   getNumberOfStudents() 
+   addStudent()
+   getNumberOfStudents() 
+ 
+   const Students = [];
+ 
+   function addStudent(name) {
+    return addStudent.name=name;
+   }
+   console.log(Students) 
+   addStudent('Josh')
+   console.log(Students) 
+   addStudent('John')
+   console.log(Students) 
+   addStudent('Ines')
+   console.log(Students) 
+   addStudent('Syrine')
+   console.log(Students) 
+   addStudent('Yahia')
+   console.log(Students) 
+ 
+   function clearStudents(name) {
+       var i=0;
+    for (var i in Students){
+        Students.POP();
+        i=i+1;
+    }
+   }
+   console.log(Students) 
+   getNumberOfStudents() 
+   clearStudents()
+   console.log(Students)
+   getNumberOfStudents()
